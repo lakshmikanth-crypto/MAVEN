daba055fff0073a60b82d82dfc2b0899316ba375
lakshmikanth-crypto <plkanth520@gmail.com>
2020-05-24 09:01:47 +0530

    sample project

---
+++ src/test/java/Sample.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Sample {
+public static void main(String[] args) throws IOException {
+	File f=new File("C:\\Users\\madhu\\eclipse-workspace\\Maven\\ExcelFiles\\data.xlsx");
+	FileInputStream fi = new FileInputStream(f);
+	Workbook w= new XSSFWorkbook(fi);
+	Sheet s=w.getSheet("Sheet1");
+	int noofrows =s.getPhysicalNumberOfRows();
+	System.out.println("Numberof rows:"+noofrows);
+	//for(int i=0;i < s.getPhysicalNumberOfRows();i++) {
+	//Row r=s.getRow(0);
+	
+}
+}
